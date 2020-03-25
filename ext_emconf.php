88d301b20a65168740850fc98e0387d8e2a765ab
Daniel Huf <dhuf@users.noreply.github.com>
2020-03-25 16:39:54 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0.0-10.2.99',
+            'typo3' => '9.5.0.0-10.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
